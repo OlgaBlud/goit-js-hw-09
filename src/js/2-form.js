b4d05c720fd559393182831e7046a94bed88ad55
Olga Blud <olga.blud@gmail.com>
2024-06-03 15:12:19 +0300

    form done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,6 +1,44 @@
-console.log('form');
+const formData = { email: '', message: '' };
 
-// Оголоси поза будь-якими функціями об’єкт formData з полями email та message, які спочатку мають порожні рядки як значення: { email: "", message: "" }.
-// Використовуй метод делегування для відстеження змін у формі через подію input. Зберігай актуальні дані з полів email та message у formData та записуй цей об’єкт у локальне сховище. Використовуй ключ "feedback-form-state" для зберігання даних у сховищі.
-// При завантаженні сторінки перевір, чи є дані у локальному сховищі. Якщо так, використовуй їх для заповнення форми та об'єкта formData. Якщо ні, залиш поля форми порожніми.
-// Перед відправленням форми переконайся, що обидва поля форми заповнені. Якщо будь-яке з полів (властивостей об’єкта formData) порожнє, показуй сповіщення з текстом «Fill please all fields». Якщо всі поля заповнені, виведи у консоль об’єкт formData з актуальними значеннями, очисти локальне сховище, об’єкт formData і поля форми.
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', getInputData);
+window.addEventListener('DOMContentLoaded', onReloadGetLSData);
+form.addEventListener('submit', validateOnSubmitBtn);
+
+function getInputData() {
+  formData.email = form.elements.email.value;
+  formData.message = form.elements.message.value;
+  const jsonFormData = JSON.stringify(formData);
+  localStorage.setItem('feedback-form-state', jsonFormData);
+}
+
+function onReloadGetLSData() {
+  const jsonDataFromLS = localStorage.getItem('feedback-form-state');
+  if (!jsonDataFromLS) {
+    return;
+  }
+  try {
+    const dataFromLS = JSON.parse(jsonDataFromLS);
+    form.elements.email.value = dataFromLS.email;
+    form.elements.message.value = dataFromLS.message;
+    formData.email = dataFromLS.email;
+    formData.message = dataFromLS.message;
+  } catch (error) {
+    console.error('Error parsing JSON from localStorage:', error);
+  }
+}
+
+function validateOnSubmitBtn(event) {
+  event.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+    formData.email = '';
+    formData.message = '';
+  }
+}
